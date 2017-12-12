75eb3f69626d870c967ef2404dc8162b1b6ff4fb
Tomas Lycken <tlycken@users.noreply.github.com>
2017-12-12 10:41:07 +0100

    Add project files.

---
+++ EFCoreOnExistingDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreOnExistingDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
